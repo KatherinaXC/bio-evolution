a8b09998b4e5393a8621d0662658c4d31af97a42
KatherinaXC <chymeraclaw@gmail.com>
2016-07-04 23:06:41 -0700

    playing with the graphics
    maybe i should actually start thinking about the UI... just maybe...

--- src/io/github/katherinaxc/bioevosim/Program.java
+++ src/io/github/katherinaxc/bioevosim/Program.java
@@ -5,35 +5,56 @@ import java.util.Scanner;
 public class Program {
 	public static void main(String[] args) {
 		Scanner console = new Scanner(System.in);
-		System.out.println("Starting board X");
+		System.out.println("Starting board X?");
 		int sizeX = console.nextInt();
-		System.out.println("Starting board Y");
+		System.out.println("Starting board Y?");
 		int sizeY = console.nextInt();
-		System.out.println("Starting board predator/prey count EACH");
+		System.out.println("Starting board predator/prey count EACH?");
 		int countEach = console.nextInt();
-		System.out.println("Starting stat lower bound");
+		System.out.println("Starting stat lower bound?");
 		int lowerBound = console.nextInt();
-		System.out.println("Starting stat upper bound");
+		System.out.println("Starting stat upper bound?");
 		int upperBound = console.nextInt();
 		System.out.println("How many iterations to run?");
 		int iterations = console.nextInt();
 		console.close();
 		Board board = new Board(sizeX, sizeY, countEach, lowerBound, upperBound);
+		initializePen();
+		drawBoard(board);
 		for (int i = 0; i < iterations; i++) {
 			board.randomize();
+			updateBoard(board);
 			board.sortByStat();
 			board.feast();
 			board.checkSurvivors();
 			board.reproduce();
-			drawBoard(board);
 		}
 	}
 
+	public static void initializePen() {
+		StdDraw.setCanvasSize(800, 800);
+		StdDraw.setScale(-.05, 1.05);
+		StdDraw.setPenRadius(0.005);
+	}
+
 	public static void drawBoard(Board board) {
-		StdDraw.setPenRadius(0.05);
-		StdDraw.setPenColor(StdDraw.BLUE);
-		StdDraw.point(0.5, 0.5);
-		StdDraw.setPenColor(StdDraw.MAGENTA);
-		StdDraw.line(0.2, 0.2, 0.8, 0.2);
+		drawCardsGrid(board.sizeX, board.sizeY);
+		StdDraw.textRight(1, -0.025, "Click or Space Key To Advance");
+		// TODO
+	}
+
+	public static void updateBoard(Board board) {
+		// TODO
+	}
+
+	public static void drawCardsGrid(int cellsX, int cellsY) {
+		double cellWidth = 1. / cellsX;
+		double cellHeight = 1. / cellsY;
+		for (int i = 0; i <= cellsX; i++) {
+			StdDraw.line(i * cellWidth, 0, i * cellWidth, 1);
+		}
+		for (int i = 0; i <= cellsY; i++) {
+			StdDraw.line(0, i * cellHeight, 1, i * cellHeight);
+		}
 	}
 }
